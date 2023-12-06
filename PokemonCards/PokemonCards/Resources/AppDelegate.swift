fc3e815a647ce5030b3c6700fe1cb8dffb6a0cd2
Berkay Ünal <berkay.unal579@gmail.com>
2023-12-06 16:57:57 +0300

    refactor(AppDelegate): update rootVC

--- PokemonCards/PokemonCards/Resources/AppDelegate.swift
+++ PokemonCards/PokemonCards/Resources/AppDelegate.swift
@@ -14,14 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     
-    let window = UIWindow(frame: UIScreen.main.bounds)
-    let navVC = UINavigationController(rootViewController: ViewController())
-    navVC.navigationBar.prefersLargeTitles = true
-    navVC.viewControllers.first?.navigationItem.largeTitleDisplayMode = .always
-    window.rootViewController = navVC
-
-    window.makeKeyAndVisible()
-    self.window = window
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    let controller = ViewController()
+    window?.rootViewController = controller
+    window?.makeKeyAndVisible()
     return true
   }
 }
