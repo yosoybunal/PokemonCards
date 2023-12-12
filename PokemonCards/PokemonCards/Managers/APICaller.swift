850abdd5e7a2fee2ffb66732af171ca264f0d639
Berkay Ünal <berkay.unal579@gmail.com>
2023-12-12 22:39:35 +0300

    refactor(APICaller): delete unnecessary search func

--- PokemonCards/PokemonCards/Managers/APICaller.swift
+++ PokemonCards/PokemonCards/Managers/APICaller.swift
@@ -19,25 +19,6 @@ final class APICaller {
 
   // MARK: - Search API
 
-  public func search(completionHandler: @escaping (Result<SearchResultsModel, Error>) -> Void) {
-
-    guard let url = URL(string: Constants.healthBaseURL) else { return }
-
-    URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
-      guard let data = data, error == nil else { return }
-
-      do{
-        let result = try
-        //        JSONSerialization.jsonObject(with: data)
-        JSONDecoder().decode(SearchResultsModel.self, from: data)
-        completionHandler(.success(result))
-      } catch(let error) {
-        print(error.localizedDescription)
-        completionHandler(.failure(error))
-      }
-    } .resume()
-  }
-
   public func searchWithHP(with query: String, completionHandler: @escaping (Result<[Card], Error>) -> Void) {
 
     guard let url = URL(string: Constants.healthBaseURL+"\(query)") else { return }
