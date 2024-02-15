3c901da8429f30e972f8ef291b3ffe1f0dd528b0
Berkay Ünal <berkay.unal579@gmail.com>
2024-02-15 13:08:21 +0300

    refactor(TabBar): refactor to more readable version

--- PokemonCards/PokemonCards/Extensions/UserDefaults+Additions.swift
+++ PokemonCards/PokemonCards/Extensions/UserDefaults+Additions.swift
@@ -10,7 +10,6 @@ import Foundation
 extension UserDefaults {
 
   func save<T:Encodable>(customObject object: T, inKey key: String) {
-    
     let encoder = JSONEncoder()
     if let encoded = try? encoder.encode(object) {
       self.set(encoded, forKey: key)
@@ -18,7 +17,6 @@ extension UserDefaults {
   }
 
   func retrieve<T:Decodable>(object type:T.Type, fromKey key: String) -> T? {
-
     if let data = self.data(forKey: key) {
       let decoder = JSONDecoder()
       if let object = try? decoder.decode(type, from: data) {
--- PokemonCards/PokemonCards/Scenes/TabBarViewController.swift
+++ PokemonCards/PokemonCards/Scenes/TabBarViewController.swift
@@ -12,19 +12,21 @@ class TabBarViewController: UITabBarController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    let vc1 = SearchResultsViewController()
-    vc1.title = "Search"
-    let nav1 = UINavigationController(rootViewController: vc1)
-    nav1.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
-    nav1.navigationBar.prefersLargeTitles = true
+    // MARK: - Search Navigation
+    let rootViewController = SearchResultsViewController()
+    rootViewController.title = "Search"
+    let searchNavigation = UINavigationController(rootViewController: rootViewController)
+    searchNavigation.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+    searchNavigation.navigationBar.prefersLargeTitles = true
 
+    // MARK: - Favorite Navigation
     let storyboard = UIStoryboard(name: "Favorite", bundle: nil)
-    let favoriteNavigation = storyboard.instantiateInitialViewController() as? UINavigationController
-    favoriteNavigation?.tabBarItem = UITabBarItem(title: "Favorites", image: UIImage(systemName: "bolt.heart.fill"), tag: 1)
+    guard let favoriteNavigation = storyboard.instantiateInitialViewController() as? UINavigationController else { return }
+    favoriteNavigation.tabBarItem = UITabBarItem(title: "Favorites", image: UIImage(systemName: "bolt.heart.fill"), tag: 2)
+    guard let favoriteVC = favoriteNavigation.topViewController as? FavoriteViewController else { return }
+    favoriteVC.title = "Favorites"
 
-    let favoriteVC = favoriteNavigation?.topViewController as? FavoriteViewController
-    favoriteVC?.title = "Favorites"
-
-    setViewControllers([nav1, favoriteNavigation!], animated: false)
+    // MARK: - All Navigation View Controllers
+    setViewControllers([searchNavigation, favoriteNavigation], animated: false)
   }
 }
